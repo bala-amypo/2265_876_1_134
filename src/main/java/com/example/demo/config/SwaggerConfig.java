9bc36736c89aa32a2acb8e322e64d6c28ee46ce2
bala-amypo <bala@amypo.in>
2025-12-18 18:52:55 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,1 +1,18 @@
-n
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Post-Surgery Recovery Tracker API")
+                        .version("1.0"));
+    }
+}
