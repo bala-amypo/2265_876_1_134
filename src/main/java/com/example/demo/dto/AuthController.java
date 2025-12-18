460ce808e50ac68da7b4d644208657e966ce2f02
bala-amypo <bala@amypo.in>
2025-12-18 18:55:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.*;
+import com.example.demo.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        return authService.login(request);
+    }
+}
--- src/main/java/com/example/demo/security/CustomerUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomerUserDetailsService.java
@@ -1,1 +1,26 @@
-n
+package com.example.demo.security;
+
+import com.example.demo.model.AppUser;
+import com.example.demo.repository.AppUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final AppUserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        AppUser user = repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
