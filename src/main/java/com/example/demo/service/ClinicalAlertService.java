23667267b331e20d0ad3b763e296b65d7f05c75e
bala-amypo <bala@amypo.in>
2025-12-20 18:30:33 +0000

    User commit

--- src/main/java/com/example/demo/service/ClinicalAlertService.java
+++ src/main/java/com/example/demo/service/ClinicalAlertService.java
@@ -1,3 +1,7 @@
 package com.example.demo.service;
 import com.example.demo.model.ClinicalAlert;
 import java.util.List;
+
+public interface clinicalAlertService{
+    ClinicalALert resolveAlert(Long id)
+}
