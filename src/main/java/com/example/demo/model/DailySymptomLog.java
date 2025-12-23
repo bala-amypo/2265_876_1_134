71d6e4e324630d2891e014c83ccdd218f3fe9bc9
bala-amypo <bala@amypo.in>
2025-12-23 09:18:52 +0000

    User commit

--- src/main/java/com/example/demo/model/DailySymptomLog.java
+++ src/main/java/com/example/demo/model/DailySymptomLog.java
@@ -3,6 +3,7 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
